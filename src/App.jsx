bd43457bd93330dfea1019acb0584b6b07a9dfc5
Sunil Sharma <sunilpoudel1999@gmail.com>
2024-11-09 16:23:31 +0545

    fevicon

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import { Outlet } from 'react-router-dom';
+import serveFavicon from 'serve-favicon';
+
 
 function App() {
 
