987d733753dc362794ff54b8814f8c1b70f05215
Sunil Sharma <sunilpoudel1999@gmail.com>
2024-11-09 19:15:19 +0545

    images added

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -1,14 +1,14 @@
 const blogs = [
     {
         id: 1,
-        image: '/images/nepali-interest-calculator-blog1.png',
+        image: '/images/what-is-nepali-interest-calculator.webp',
         url: '/what-is-nepali-interest-calculator',
-        imagealt: 'How does nepali interest calculator works',
-        title: " What is nepali interest calculator?",
+        imagealt: 'what-is-nepali-interest-calculator',
+        title: " What is nepali interest calculator ?",
         content: `<p>The <strong>Nepali Interest Calculator</strong> is a tool designed to make traditional Nepali interest calculations easier and more understandable. It was developed to assist those who may be confused by local interest practices—especially the unique systems like "1 rupee," "1.5 rupee," or "2 rupee" rates, commonly used in Nepal's traditional lending and borrowing frameworks.</p>
         <br>
 
-    <h2 class="text-2xl font-bold">Why the Nepali Interest Calculator?</h2>
+    <h2 class="text-2xl font-bold">Why the Nepali Interest Calculator ?</h2>
     <br>
     <p>Traditional Nepali interest rates can be perplexing, especially for individuals more accustomed to modern bank rates. In Nepal’s local lending system, phrases like "1 rupee" or "2 rupee" denote particular interest rates that are not directly comparable to the standard "percentage per annum" rate in banking systems. For instance, someone familiar with a 10% annual interest might find it challenging to interpret or calculate a "1 rupee" interest rate accurately.</p>
     <p>The <strong>Nepali Interest Calculator</strong> bridges this gap. It converts these traditional rates into understandable formats, like simple and compound interest. This helps people, particularly those familiar with modern banking rates, to comprehend and calculate local interest terms effectively.</p>
@@ -24,17 +24,18 @@ const blogs = [
     <h2 class="text-2xl font-bold">Who Can Benefit from the Nepali Interest Calculator?</h2>
     <br>
     <p>The tool is beneficial to anyone who finds traditional Nepali interest rates confusing or hard to convert into more familiar formats. It’s particularly useful for those who understand standard banking interest rates and need a way to translate these complex traditional rates into simpler terms.</p>`,
-        keywords: ['Nepali Interest Calculator', 'Interest', 'Calculator'],
-        publisheddate: '1 October 2024',
-        lastupdate: '2 October 2024',
+        keywords: ['Nepali Interest Calculator', 'what is nepali interest calculator', 'Calculator'],
+        publisheddate: '10 November 2024',
+        lastupdate: '11 November 2024',
     },
     {
         id: 2,
-        image: '/images/nepali-interest-calculator-blog1.png',
+        image: '/images/how-does-nepali-interest-calculator-works.webp',
+        imagealt: 'how-does-nepali-interest-calculator-works',
         url: '/how-does-nepali-interest-calculator-works',
         title: "How does nepali interest calculator works ?",
         content: `<p>The <strong>Nepali Interest Calculator</strong> is a practical tool that helps users easily calculate interest based on traditional Nepali rates, such as "1 rupee," "1.5 rupee," or "2 rupee" rates. By translating these local interest terms into familiar formats like simple and compound interest, this calculator simplifies financial planning for both traditional lenders and modern-day users.</p>
-<br>
+        <br>
     <h2 class="text-2xl font-bold">Understanding Traditional Nepali Interest Rates</h2>
     <br>
     <p>In Nepal, local lending practices often use terms like "1 rupee" or "2 rupee" rates. These terms refer to specific interest calculations that can be challenging for those who typically use a percentage-per-annum model. The Nepali Interest Calculator bridges this gap by converting these traditional rates into formats that align with modern banking standards, making the calculations accessible to a wider audience.</p>
@@ -75,13 +76,14 @@ const blogs = [
     <br>
      <p>In summary, the <strong>Nepali Interest Calculator</strong> serves as a bridge between traditional Nepali lending systems and modern banking practices. By providing clear and accurate conversions, it enables anyone to understand and calculate local interest terms with confidence.</p>
     `,
-        keywords: ['Nepali Interest Calculator', 'Interest', 'Calculator'],
-        publisheddate: '2 October 2024',
-        lastupdate: '3 October 2024',
+        keywords: ['How does nepali interest calculator works', 'Interest', 'Calculator'],
+        publisheddate: '11 November 2024',
+        lastupdate: '12 November 2024',
     },
     {
         id: 3,
-        image: '/images/nepali-interest-calculator-blog1.png',
+        image: '/images/how-to-use-nepali-interest-calculator.webp',
+        imagealt: 'how-to-use-nepali-interest-calculator',
         url: '/how-to-use-nepali-interest-calculator',
         title: "How to use nepali interest calculator ?",
         content: `<p>The <strong>Nepali Interest Calculator</strong> is a practical tool that helps users easily calculate interest based on traditional Nepali rates, such as "1 rupee," "1.5 rupee," or "2 rupee" rates. By translating these local interest terms into familiar formats like simple and compound interest, this calculator simplifies financial planning for both traditional lenders and modern-day users.</p>
@@ -131,12 +133,13 @@ const blogs = [
     <br>
     <p>The <strong>Nepali Interest Calculator</strong> is a valuable tool that brings clarity to traditional interest calculations. Whether you’re managing personal finances, lending, or borrowing, this calculator simplifies the process, making it easy to understand and compute traditional Nepali interest rates in a modern format.</p>`,
         keywords: ['Nepali Interest Calculator', 'Interest', 'Calculator'],
-        publisheddate: '2 October 2024',
-        lastupdate: '3 October 2024',
+        publisheddate: '13 November 2024',
+        lastupdate: '13 November 2024',
     },
     {
         id: 4,
-        image: '/images/nepali-interest-calculator-blog1.png',
+        image: '/images/benefits-of-using-nepali-interest-calculator.webp',
+        imagealt: 'benefits-of-using-nepali-interest-calculator',
         url: '/benefits-of-using-nepali-interest-calculator',
         title: "Benefits of using Nepali Interest Calculator",
         content: `    <p>The <strong>Nepali Interest Calculator</strong> is designed to make traditional Nepali interest calculations accessible and easy to understand. This calculator is particularly helpful for those needing an efficient way to work with local interest terms, such as "1 rupee" or "2 rupee" rates, while offering additional benefits that make it a valuable tool for everyday financial calculations.</p>
@@ -181,8 +184,49 @@ const blogs = [
     <br>
     <p>The <strong>Nepali Interest Calculator</strong> is more than just a calculator; it’s a powerful resource that simplifies traditional interest calculations, enhances accuracy, and supports financial decision-making. Whether for personal or professional use, this calculator is a valuable tool for anyone working with Nepali interest rates.</p>`,
         keywords: ['Nepali Interest Calculator', 'Interest', 'Calculator'],
-        publisheddate: '2 October 2024',
-        lastupdate: '3 October 2024',
+        publisheddate: '14 November 2024',
+        lastupdate: '15 November 2024',
+    },
+    {
+        id: 5,
+        image: '/images/what-is-1-rupee-interest-rate.webp',
+        imagealt: 'what-is-1-rupee-interest-rate',
+        url: '/what-is-1-rupee-interest-rate',  
+        title: "What is 1 rupee interest rate ?",
+        content: `<p>The <strong>"1 Rupee" interest rate</strong> is a traditional method of calculating interest in Nepal, commonly used in informal lending. Unlike standard banking rates based on annual percentages, a "1 rupee" interest rate means that for every 100 rupees borrowed, 1 rupee is charged as interest per month.</p>
+<br>
+
+<h2 class="text-2xl font-bold">How the "1 Rupee" Interest Rate Works</h2>
+<br>
+<p>This type of interest calculation is often based on a monthly rate:</p>
+<ul>
+    <li><strong>1 Rupee per 100 Rupees per Month:</strong> This translates to a 1% interest rate per month on the principal.</li>
+    <li><strong>Annual Equivalent:</strong> If applied monthly without compounding, it roughly amounts to a 12% annual interest rate.</li>
+</ul>
+<br>
+<p>Calculate 1 Rupee Interest Easily using <a href="https://nepaliintrestcalculator.vercel.app/"><strong><u>Nepali Interest Calculator</u></strong></a></p>
+</br>
+<p>For example:</p>
+<ul>
+    <li>If someone borrows 10,000 rupees at a 1 rupee interest rate, they would owe 100 rupees in interest each month.</li>
+    <li>Over a year, this would add up to 1,200 rupees in interest (assuming no compounding).</li>
+</ul>
+<br>
+
+<h2 class="text-2xl font-bold">Variations of the Nepali Interest Rate</h2>
+<br>
+<p>Other traditional Nepali interest rates include:</p>
+<ul>
+    <li><strong>1.5 Rupee Rate:</strong> 1.5 rupees per 100 per month, or 1.5% monthly, which equates to approximately 18% per year.</li>
+    <li><strong>2 Rupee Rate:</strong> 2 rupees per 100 per month, or 2% monthly, amounting to around 24% per year.</li>
+</ul>
+<br>
+
+<p>These traditional interest rates are commonly used in local lending systems, particularly in rural areas or for smaller financial transactions. The <strong>Nepali Interest Calculator</strong> is especially useful for translating these unique rates into more standard formats, making calculations simpler and more understandable for modern users.</p>
+`,
+        keywords: ['Nepali Interest Calculator', '1 Rupee Interest Rate', '1 rupee interest'],
+        publisheddate: '14 November 2024',
+        lastupdate: '15 November 2024',
     }
 
 ]
